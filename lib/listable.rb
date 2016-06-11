9a43885b86df8e82e3b0a45bff177c2770fe9039
Kareem Rady <kareem_rady@yahoo.com>
2016-06-11 20:57:08 +0000

    Refactored format_priority and added to listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -17,4 +17,12 @@ module Listable
       due ? due.strftime("%D") : "No due date"
     end
   end
+
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
 end
